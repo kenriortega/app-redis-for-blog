1f014a57822f842c7592fff3744893b3f5fe6cc8
Kenrique Ortega <53656046+kenriortega@users.noreply.github.com>
2021-11-05 12:08:02 -0400

    add recomendantion method DrainBody

--- main.go
+++ main.go
@@ -104,7 +104,7 @@ func getCoins(
 	if err != nil {
 		return
 	}
-	defer res.Body.Close()
+	defer DrainBody(res.Body)
 
 	coins := []Coin{}
 	if err = json.NewDecoder(res.Body).Decode(&coins); err != nil {
@@ -139,3 +139,18 @@ func GetRedisDbClient(ctx context.Context) *redis.Client {
 
 	return clientInstance
 }
+
+func DrainBody(respBody io.ReadCloser) {
+	// Callers should close resp.Body when done reading from it.
+	// If resp.Body is not closed, the Client's underlying RoundTripper
+	// (typically Transport) may not be able to re-use a persistent TCP
+	// connection to the server for a subsequent "keep-alive" request.
+	if respBody != nil {
+	  // Drain any remaining Body and then close the connection.
+	  // Without this closing connection would disallow re-using
+	  // the same connection for future uses.
+	  //  - http://stackoverflow.com/a/17961593/4465767
+	  defer respBody.Close()
+	  _, _ = io.Copy(ioutil.Discard, respBody)
+	}
+  }
