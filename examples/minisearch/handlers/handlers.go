ae1e1bfcf7e8e19490beeff7874e92e66c2c4830
kenriortega <kenriortega@gmail.com>
2021-11-14 22:20:58 -0500

    ref: implement awesome logic to parse result data from redis to and simple map

--- examples/minisearch/handlers/handlers.go
+++ examples/minisearch/handlers/handlers.go
@@ -56,10 +56,34 @@ func (h *Handler) FindPizzasByCountry(w http.ResponseWriter, r *http.Request) {
 		0,
 		100,
 	).Result()
+	total := result.([]interface{})[0]
+	docs := result.([]interface{})[1:]
+	var values []interface{}
+	for i, doc := range docs {
+		if i%2 != 0 {
+			value := make(map[string]interface{})
+			var k, v string
+			for j, it := range doc.([]interface{}) {
+				if j%2 == 0 {
+					k = it.(string)
+				}
+				if j%2 != 0 {
+					v = it.(string)
+				}
+				value[k] = v
+			}
+			values = append(values, value)
+		}
+	}
+
+	data := make(map[string]interface{})
+	data["total"] = total
+	data["total_peer_page"] = len(values)
+	data["docs"] = values
 	if err != nil {
 		writeResponse(w, http.StatusBadRequest, err)
 	} else {
-		writeResponse(w, http.StatusOK, result)
+		writeResponse(w, http.StatusOK, data)
 	}
 }
 
