a96408a709f9e14a9b18c79b08eefc321c3ab5ff
kenriortega <kenriortega@gmail.com>
2021-11-14 20:09:48 -0500

    ref: Add model pizza

--- examples/minisearch/main.go
+++ examples/minisearch/main.go
@@ -4,55 +4,16 @@ import (
 	"app/pkg/db"
 	"context"
 	"encoding/csv"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"app/examples/minisearch/model"
+
 	"github.com/go-redis/redis/v8"
 )
-
-// PizzaRestaurant ...
-type PizzaRestaurant struct {
-	ID              string `json:"id,omitempty"`
-	DateAdded       string `json:"date_added,omitempty"`
-	Address         string `json:"address,omitempty"`
-	Category        string `json:"category,omitempty"`
-	PrimaryCategory string `json:"primary_category,omitempty"`
-	City            string `json:"city,omitempty"`
-	Country         string `json:"country,omitempty"`
-	Location        string `json:"location,omitempty"`
-	PageURL         string `json:"page_url,omitempty"`
-	AmmountMAX      string `json:"ammount_max,omitempty"`
-	Currency        string `json:"currency,omitempty"`
-	Description     string `json:"description,omitempty"`
-	Name            string `json:"name,omitempty"`
-}
-
-// ToJSON ...
-func (p *PizzaRestaurant) ToJSON() string {
-	bytes, err := json.Marshal(p)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	return string(bytes)
-}
-
-// ToMAP ...
-func (p *PizzaRestaurant) ToMAP() (toHashMap map[string]interface{}, err error) {
-
-	fromStruct, err := json.Marshal(p)
-	if err != nil {
-		return nil, err
-	}
-	if err := json.Unmarshal(fromStruct, &toHashMap); err != nil {
-		return toHashMap, err
-	}
-
-	return toHashMap, nil
-}
 
 var (
 	action string
@@ -69,6 +30,7 @@ func main() {
 	rdb := db.GetRedisDbClient(context.Background())
 	// cmd managements
 	switch action {
+
 	case "seed":
 		start := time.Now()
 		var path, _ = os.Getwd()
@@ -103,26 +65,11 @@ func ingestDataToRedis(
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, line := range csvLines {
-		pizzaR := PizzaRestaurant{
-			ID:              line[0],
-			DateAdded:       line[1],
-			Address:         line[3],
-			Category:        line[4],
-			PrimaryCategory: line[5],
-			City:            line[6],
-			Country:         line[7],
-			// redis geo types are declared "longitude,latitude"
-			Location:    fmt.Sprintf("%s,%s", line[10], line[9]),
-			PageURL:     line[11],
-			AmmountMAX:  line[12],
-			Currency:    line[14],
-			Description: line[16],
-			Name:        line[17],
-		}
+	for index, line := range csvLines[1:] {
 
-		// Redis Insgestion Proccess
-		key := fmt.Sprintf(`pizza:%s`, pizzaR.ID)
+		pizzaR := model.NewPizzaR(line)
+
+		key := fmt.Sprintf(`pizza:%s_%d`, pizzaR.ID, index)
 		value, err := pizzaR.ToMAP()
 		if err != nil {
 			log.Fatal(err)
